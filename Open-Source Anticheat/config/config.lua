5e5ffbf098e8064b89e93f15e4d219effa2c872a
V01D <138397807+flowzilla@users.noreply.github.com>
2024-11-22 20:57:24 +0100

    Blackweek sale - start now your own project

--- Open-Source Anticheat/config/config.lua
+++ Open-Source Anticheat/config/config.lua
@@ -1,6 +1,6 @@
 ES = {}
 
--- Full version on our Discord https://discord.gg/8cwwavkBD6
+-- Full version on our Discord https://discord.gg/authify
 
 ES.LicenseKey = ""
 ES.ConfigSecret = "" -- You can find a version with Lua Config Files on our discord
