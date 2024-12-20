0d754ee52317bac373ef3ae1a80f5296209267da
V01D <138397807+flowzilla@users.noreply.github.com>
2024-12-20 20:56:18 +0100

    merry christmas

--- Open-Source Anticheat/config/config.lua
+++ Open-Source Anticheat/config/config.lua
@@ -1,6 +1,6 @@
 ES = {}
 
--- Full version on our Discord https://discord.gg/authify
+-- Full version on our Discord https://e-z.bio/auth
 
 ES.LicenseKey = ""
 ES.ConfigSecret = "" -- You can find a version with Lua Config Files on our discord
